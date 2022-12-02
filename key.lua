4c1d28ff4cd1252d9c2ad9e5b2d1d8d5c16c486a
Provide <87718468+SomebodyYouDidntMeet@users.noreply.github.com>
2022-12-02 11:21:37 +0200

    Create key.lua

---
+++ key.lua
@@ -0,0 +1,8 @@
+local key = loadstring(game:HttpGet("https://pastebin.com/raw/Mgha2snU"))()
+
+key:MakeUI({
+  Title = "Provide Hub",
+  KeyLinkName = "ProvideHub",
+  DiscordID = "NMfCm8zVFs",
+  ScriptLink = "https://pastebin.com/raw/PHh1aFmW"
+})
